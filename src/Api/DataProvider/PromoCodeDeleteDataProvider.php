b61b9424f7a7614d46f5caf0962e12bdfda38270
bartek <bartlomiejmazurek9@gmail.com>
2024-03-15 22:21:52 +0100

    Log delete operation

--- src/Api/DataProvider/PromoCodeDeleteDataProvider.php
+++ src/Api/DataProvider/PromoCodeDeleteDataProvider.php
@@ -7,17 +7,22 @@ namespace App\Api\DataProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Api\Dto\PromoCode\PromoCodeDeleteOutput;
+use App\Event\PromoCodeHistory\PromoCodeLogEvent;
 use App\Repository\PromoCodeRepository;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PromoCodeDeleteDataProvider implements ProviderInterface
 {
     private PromoCodeRepository $promoCodeRepository;
+    private EventDispatcherInterface $eventDispatcher;
 
     public function __construct(
-        PromoCodeRepository $promoCodeRepository
+        PromoCodeRepository $promoCodeRepository,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->promoCodeRepository = $promoCodeRepository;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function provide(
@@ -37,7 +42,15 @@ class PromoCodeDeleteDataProvider implements ProviderInterface
             throw new NotFoundHttpException(message: 'Promo code not available');
         }
 
+        $promoCodeId = $promoCode->getId();
         $this->promoCodeRepository->remove(promoCode: $promoCode);
+
+        $this->eventDispatcher->dispatch(
+            PromoCodeLogEvent::createFromData(
+                operation: PromoCodeLogEvent::DELETE,
+                data: ['id' => $promoCodeId]
+            )
+        );
 
         return new PromoCodeDeleteOutput();
     }
