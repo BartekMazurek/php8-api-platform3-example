bd67aaa2ea88f905d5c3410b7ad202226d3cc6c1
bartek <bartlomiejmazurek9@gmail.com>
2024-03-15 21:50:53 +0100

    Updated start script - wait 2 seconds till docker container setup

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 docker compose up -d
+
+sleep 2
+
+docker exec promo_php composer install
 docker exec promo_php bin/console --no-interaction doctrine:migrations:migrate
 docker exec promo_php sh -c 'cd /var/www/app/postman && newman run collection.json -e environment.json'
